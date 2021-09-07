2f7f388c251f618b6ebfbb7c1badb8be262ab6a6
Prahlad Inala <prahlad2018@gmail.com>
2021-09-07 19:16:10 +0530

    jinja template - variables, if, else, elif

--- projects/views.py
+++ projects/views.py
@@ -5,7 +5,15 @@ from django.http import HttpResponse
 def projects(request):
     # return HttpResponse('Here are our projects')
     # return render(request, 'projects.html')
-    return render(request, 'projects/projects.html')
+    # PASSING VARIABLES
+    # msg = 'Hello, you are on the projects page'
+    # return render(request, 'projects/projects.html', {'message': msg})
+    page = 'projects'
+    number = 10
+    context = {'page':page, 'number': number}
+    return render(request, 'projects/projects.html', context)
+
+    # return render(request, 'projects/projects.html', {'anyname_here_to_call_in_html': msg})
 
 def project(request, pk):
     # return HttpResponse('Single Project' + ' ' + str(pk))
